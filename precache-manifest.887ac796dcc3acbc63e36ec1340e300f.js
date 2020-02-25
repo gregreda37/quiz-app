6d17ad8f963185e417b87059ebb52d967cb4ca57
Greg Reda <54119966+gregreda37@users.noreply.github.com>
2020-02-25 10:38:04 -0500

    Updates

---
+++ precache-manifest.887ac796dcc3acbc63e36ec1340e300f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5991d5e66cb6dcf6eed6144941e892eb",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "6d866fd91f6ba6118557",
+    "url": "/quiz-app/static/css/main.a40a39a8.chunk.css"
+  },
+  {
+    "revision": "c22469f986aa676b56f1",
+    "url": "/quiz-app/static/js/2.6561203d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/quiz-app/static/js/2.6561203d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d866fd91f6ba6118557",
+    "url": "/quiz-app/static/js/main.9f25260f.chunk.js"
+  },
+  {
+    "revision": "b37ff92f430aaf9d3d9e",
+    "url": "/quiz-app/static/js/runtime-main.115cbb33.js"
+  }
+]);
